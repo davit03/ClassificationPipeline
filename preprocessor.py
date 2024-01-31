a2cedc980261900c6316fbde943b554611c05e15
lina-mejlumyan <157614447+lina-mejlumyan@users.noreply.github.com>
2024-01-31 16:28:05 +0400

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,11 +1,11 @@
 import pickle
 import numpy as np
 import pandas as pd
+from sklearn.base import TransformerMixin
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler, MinMaxScaler, RobustScaler, VarianceThreshold, SelectKBest
 
-
-class Preprocessor:
+class Preprocessor(TransformerMixin):
     def __init__(self, threshold=0.001, n_neighbors=5, degree=2, k_best=10):
         self.threshold = threshold
         self.n_neighbors = n_neighbors
@@ -45,11 +45,12 @@ class Preprocessor:
         with open(filename, 'wb') as file:
             pickle.dump(models, file)
 
-    def fit(self, x, use_saved_model=False, save_model=False, filename="preprocessor_models.pkl"):
+    def fit(self, X, use_saved_model=False, save_model=False, filename="preprocessor_models.pkl"):
         if use_saved_model:
             self.load_model(filename)
         else:
-            data = x.drop(columns=['SAPS-I', 'SOFA', 'Length_of_stay', 'Survival'])
+            y = X[['SAPS-I', 'SOFA', 'Length_of_stay', 'Survival']]  # Separate target variable
+            data = X.drop(columns=['SAPS-I', 'SOFA', 'Length_of_stay', 'Survival'])
 
             self.imputer = KNNImputer(n_neighbors=self.n_neighbors)
             self.poly_features = PolynomialFeatures(degree=self.degree, include_bias=False)
@@ -59,17 +60,13 @@ class Preprocessor:
             self.variance_selector = VarianceThreshold(threshold=self.threshold)
             self.k_best_selector = SelectKBest(k=self.k_best)
 
-            self.imputer.fit(data)
             imputed_data = self.imputer.fit_transform(data)
-
-            self.poly_features.fit(imputed_data)
             data_poly = self.poly_features.fit_transform(imputed_data)
-
-            self.scaler.fit(data_poly)
-            self.min_max_scaler.fit(data_poly)
-            self.robust_scaler.fit(data_poly)
-            self.variance_selector.fit(data_poly)
-            self.k_best_selector.fit(data_poly)
+            data_poly_scaled = self.scaler.fit_transform(data_poly)
+            data_poly_min_max_scaled = self.min_max_scaler.fit_transform(data_poly_scaled)
+            data_poly_robust_scaled = self.robust_scaler.fit_transform(data_poly_min_max_scaled)
+            data_poly_variance_selected = self.variance_selector.fit_transform(data_poly_robust_scaled)
+            data_poly_k_best_selected = self.k_best_selector.fit_transform(data_poly_variance_selected)
 
         if save_model:
             self.save_model(filename)
@@ -79,27 +76,28 @@ class Preprocessor:
         if self.scaler is None or self.imputer is None or self.poly_features is None:
             raise ValueError("Preprocessor has not been fitted. Call 'fit' method first.")
 
+        y = X[['SAPS-I', 'SOFA', 'Length_of_stay', 'Survival']]
+        # Transform features
         imputed_x = self.imputer.transform(data)
         poly_x = self.poly_features.transform(imputed_x)
         poly_columns = self.poly_features.get_feature_names_out(data.columns)
         poly_df = pd.DataFrame(poly_x, columns=poly_columns)
         X_scaled = self.scaler.transform(poly_df)
 
-        # Features to include
-        features = [
+        feature_transformers = [
             ('min_max_scaler', self.min_max_scaler),
             ('robust_scaler', self.robust_scaler),
             ('variance_selector', self.variance_selector),
             ('k_best_selector', self.k_best_selector),
-            ('poly_features', PolynomialFeatures(degree=self.degree, include_bias=False)),
-            ('standard_scaler', StandardScaler()),
-            ('knn_imputer', KNNImputer(n_neighbors=self.n_neighbors))
+            ('poly_features', self.poly_features),
+            ('standard_scaler', self.scaler),
+            ('knn_imputer', self.imputer)
         ]
 
-        additional_dfs = [transformer.transform(X_scaled) for _, transformer in features if transformer]
+        additional_dfs = [transformer.transform(X_scaled) for _, transformer in feature_transformers if transformer]
 
         if additional_dfs:
             X_scaled = pd.concat([X_scaled] + additional_dfs, axis=1)
 
-        df_result = pd.DataFrame(columns=poly_columns, data=X_scaled)
+        df_result = pd.concat([y, pd.DataFrame(columns=poly_columns, data=X_scaled)], axis=1)
         return df_result
