5f48b9938b2521c7c6af2960225a007cff2e0f61
Davit Mkrtchyan <davitmkrtchyan2003@gmail.com>
2024-02-01 12:37:33 +0400

    final version of preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,25 +1,25 @@
-import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler, MinMaxScaler
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class Preprocessor(BaseEstimator, TransformerMixin):
-    def __init__(self, scaler="standard", degree=2, strategy="mean", threshold=3, remove_outliers=True, fill_na=True):
+    def __init__(self, scaler="standard", degree=2, strategy="mean", fill_na=True):
         if scaler == "standard":
             self.scaler = StandardScaler()
         elif scaler == "minmax":
             self.scaler = MinMaxScaler()
         else:
-            raise ValueError("Invalid scaler type, must be 'standard' or 'minmax'")
-        self.remove_outliers = remove_outliers
-        self.threshold = threshold
+            # to deal with the problem with make_pipeline
+            self.scaler = scaler
         self.fill_na = fill_na
-        self.imputer = SimpleImputer(strategy=strategy)
-        self.poly_features = PolynomialFeatures(degree=degree)
+        self.degree = degree
+        self.strategy = strategy
+        self.imputer = SimpleImputer(strategy=self.strategy)
+        self.poly_features = PolynomialFeatures(degree=self.degree)
 
     def fit(self, X, y=None):
         if self.fill_na:
-            X_imputed = self.imputer.fit_transform()
+            X_imputed = self.imputer.fit_transform(X)
         else:
             X_imputed = X.copy()
         X_poly = self.poly_features.fit_transform(X_imputed)
@@ -33,9 +33,4 @@ class Preprocessor(BaseEstimator, TransformerMixin):
             X_imputed = X.copy()
         X_poly = self.poly_features.transform(X_imputed)
         X_scaled = self.scaler.transform(X_poly)
-        if self.remove_outliers:
-            # z score method to remove outliers with std greater than or less than threshold
-            z_scores = np.abs((X - X.mean(axis=0))/X.std(axis=0))
-            mask = (z_scores < self.threshold).all(axis=1)
-            X_scaled = X_scaled[mask, :]
         return X_scaled
