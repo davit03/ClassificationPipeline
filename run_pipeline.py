2466dbb5fce76bc3af4ef7157fcc72f4cf47d9c0
diana-esaian <90722094+diana-esaian@users.noreply.github.com>
2024-02-01 14:26:58 +0400

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,6 @@
 import argparse
 import pickle
+import json
 import pandas as pd
 from model import Model
 from preprocessor import Preprocessor
@@ -16,21 +17,22 @@ class Pipeline:
         dataset = pd.read_csv(datapath)
         targets = dataset['In-hospital_death']
         features = dataset.drop(columns=['In-hospital_death'])
-
-        # if test mode, previously saved models are used 
+                
         if self.test_mode:
-            # loading trained model
             with open('model.pkl', 'rb') as file:
                 loaded_model = pickle.load(file)
             self.model = loaded_model
-            # loading preprocessor model
+            
             with open('preprocess_model.pkl', 'rb') as f:
                 prep_model = pickle.load(f)
             self.preprocessor = prep_model
             
             preprocessed_features = self.preprocessor.transform(features)
-            self.model.predict(preprocessed_features)
-        
+            results = self.model.predict(preprocessed_features)
+            dictionary = {'predict_probas': results[0], 'threshold': results[1]}
+            with open("predictions.json", "w") as outfile: 
+                json.dump(dictionary, outfile)
+            
         else:
             X_train, X_test, y_train, y_test = train_test_split(features, targets, test_size=0.2)
             
@@ -39,7 +41,11 @@ class Pipeline:
             preprocessed_test_features = self.preprocessor.transform(X_test)
             
             self.model.fit(preprocessed_features, y_train)
-            self.model.predict(preprocessed_test_features)  
+            results = self.model.predict(preprocessed_test_features)  
+            
+            dictionary = {'predict_probas': results[0], 'threshold': results[1]}
+            with open("predictions.json", "w") as outfile: 
+                json.dump(dictionary, outfile)
             
             if self.save_models:
                 pickle.dump(self.preprocessor, open('preprocess_model.pkl', 'wb'))                
