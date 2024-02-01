1d37c9007a80c9a9f2f6f977e59d529ace480078
Davit Mkrtchyan <davitmkrtchyan2003@gmail.com>
2024-02-01 14:39:51 +0400

    finall version of model.py

--- model.py
+++ model.py
@@ -1,52 +1,29 @@
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import ComplementNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import StackingClassifier
+from sklearn.pipeline import make_pipeline
 from preprocessor import Preprocessor
-from sklearn import metrics
 class Model():
+    # class_weights were chosen based on compute_class_weight function and also GridSearchCV
     def __init__(self):
-        self.model = KNeighborsClassifier()
-        self.model = LogisticRegression()
-        self.model = GaussianNB()
-        self.model = LinearDiscriminantAnalysis()
-        self.model = QuadraticDiscriminantAnalysis()
-        self.model = SVC(C=1e8, kernel='poly', probability=True)
-        self.model = DecisionTreeClassifier()
-        self.model = RandomForestClassifier()
-        self.model = BaggingClassifier()
-        self.model = GradientBoostingClassifier()
+        estimators = [
+            ('nb', make_pipeline(Preprocessor(scaler="minmax", degree=1),
+                                 ComplementNB(alpha=0.0001))),
+            ('lr', make_pipeline(Preprocessor(),
+                                 LogisticRegression(max_iter=1000, class_weight="balanced"))),
+            ('dt', DecisionTreeClassifier(class_weight={0: 0.5, 1: 3.5}, random_state=21)),
+            ('lda', make_pipeline(Preprocessor(degree=1),LinearDiscriminantAnalysis())),
+        ]
+
+        self.model = StackingClassifier(estimators=estimators,
+                                        final_estimator=RandomForestClassifier(class_weight={0: 3.7, 1: 0.3}, random_state=21),
+                                        cv=5)
     def fit(self, X, y):
         self.model.fit(X, y)
-    def predict(self, X):
-        pass
-    def score(self, X, y):
-        pred = self.model.predict(X)
-        fpr, tpr, _ = metrics.roc_curve(y, pred)
-        tn, fp, fn, tp = metrics.confusion_matrix(y, pred).ravel()
-        print("Accuracy: %.2f" % (100 * (tp + tn) / (tn + fp + fn + tp)))
-        print("Sensitivity: %.3f" % (tp / (tp + fn)))
-        print("Specificity: %.3f" % (tn / (tn + fp)))
-        print("AUC: %.3f" % (metrics.auc(fpr, tpr)))
-        print("MCC: %.3f" % (metrics.matthews_corrcoef(y, pred)))
 
-data = pd.read_csv("Survival_dataset.csv")
-X = data.drop("In-hospital_death", axis = 1)
-y = data["In-hospital_death"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-preprocessor = Preprocessor()
-preprocessor.fit(X_train)
-X_train = preprocessor.transform(X_train)
-X_test = preprocessor.transform(X_test)
-model = Model()
-model.fit(X_train, y_train)
-model.score(X_test, y_test)
+    def predict(self, X):
+        y_probs = self.model.predict_proba(X)[:,1]
+        return y_probs, 0.5
